ce4d693b91a5e821e406642e2a6be7e735a35d3e
Sira <siraphop_2538@hotmail.com>
2022-11-22 01:31:41 +1100

    08-Behind-the-Scenes Completed

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,136 @@
 'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
+// printAge();
+
+// const myName = 'Jonas';
+
+// if (myName === 'Jonas') {
+//   const job = 'teacher';
+//   console.log(`Jonas is a ${job}`);
+//   const age = 2037 - 1989;
+//   console.log(age);
+//   // ;
+//   // console.log(x);
+// }
+
+//Varaibles hositing
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// //Functions
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// //Temporal deadzone beacuse of const, however even if we use var it still would not be working properly
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// //Example
+// console.log(numProducts);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
+// }
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// var firstName = 'Matilda';
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     // console.log(this);
+//     console.log(2037 - this.year);
+
+//     //use self or arrow function
+//     const self = this;
+//     const isMill = function () {
+//       console.log(self.year >= 1981 && self.year <= 1996);
+//     };
+//     isMill();
+//   },
+
+//   greet: () => {
+//     console.log(this);
+//     console.log(`Hey ${this.firstName}`);
+//   },
+// };
+
+// jonas.greet();
+// jonas.calcAge();
+
+// //arguemnts keyword only exist in function
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+
+// addExpr(2, 5);
+
+// // var addArrow = (a, b) => a + b;
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
